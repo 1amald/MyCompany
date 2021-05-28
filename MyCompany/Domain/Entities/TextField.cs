2e830c2718e37d8f0d759163ee4ad15e3850d352
iamald <iamald@yandex.ru>
2021-05-29 01:45:22 +0300

    done

---
+++ MyCompany/Domain/Entities/TextField.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class TextField : EntityBase
+    {
+        [Required]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Page name")]
+        public override string Title { get; set; } = "Information page";
+
+        [Display(Name = "Content")]
+        public override string Text { get; set; } = "Content is filled in by the administrator";
+    }
+}
